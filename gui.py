12e993618918e4b694295b84a88db56e434a26ce
IonAir1 <ionair.ironwood@gmail.com>
2023-05-31 10:36:17 +0800

    added saving and loading of config to gui

--- gui.py
+++ gui.py
@@ -1,6 +1,10 @@
 from main import Config
-import tkinter as tk
+from main import SimpleQR
 from tkinter import ttk
+import tkinter as tk
+import threading
+import webbrowser
+import os
 
 #root window
 root = tk.Tk()
@@ -8,9 +12,16 @@ root.title('SimpleQR')
 root.geometry('500x800+50+50')
 root.minsize(500, 500)
 
+config = Config('cfg.ini')
+config.load()
+link_var = tk.StringVar(root, config.url)
+invert_var = tk.StringVar(root, config.invert)
 
-link_var = tk.StringVar(root)
-invert_var = tk.StringVar(root)
+
+def on_closing():
+    config.save('url', link_var.get())
+    config.save('invert', bool(int(invert_var.get())))
+    root.destroy()
 
 
 #title
@@ -22,7 +33,7 @@ title.pack(fill='x', padx=5, pady=5)
 names_frame = ttk.LabelFrame(root, text='Names')
 names_frame.pack(padx=5, pady=5, expand=True, fill='y')
 names_text = tk.Text(names_frame, height=8, takefocus=False)
-names_text.pack(padx=10, expand=True, fill='y')
+names_text.pack(padx=10, pady=(0, 10), expand=True, fill='y')
 
 
 #forms links
@@ -30,7 +41,7 @@ fl = ttk.LabelFrame(root, text='G Forms Prefill link') #forms link frame
 fl.pack(fill='x', padx=5, pady=5)
 
 fl_entry = ttk.Entry(fl, textvariable=link_var, takefocus=False) #forms link entry input
-# fl_entry.bind("<FocusOut>", lambda event: config_instance.save('forms_link', fl_var.get(), True))
+fl_entry.bind("<FocusOut>", lambda event: config.save('url', link_var.get()))
 fl_entry.bind('<Control-a>', lambda x: fl_entry.selection_range(0, 'end') or "break")
 fl_entry.pack(padx=10, pady=10, expand=True, fill='x')
 
@@ -38,10 +49,10 @@ fl_entry.pack(padx=10, pady=10, expand=True, fill='x')
 #invert color checkbox
 ic = ttk.Checkbutton(root,
                 text='Invert Color',
-                command=lambda: print("AAAAAAA"),
+                command=lambda: config.save('invert', bool(int(invert_var.get()))),
                 variable=invert_var,
-                onvalue=True,
-                offvalue=False,
+                onvalue=1,
+                offvalue=0,
                 takefocus=False)
 ic.pack(padx=10, pady=5, anchor='nw')
 
@@ -71,4 +82,5 @@ pb = ttk.Progressbar( #progress bar
 )
 
 
+root.protocol("WM_DELETE_WINDOW", on_closing)
 root.mainloop()
