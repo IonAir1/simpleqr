362905b72e414de39ad70da8978e4921b804d83b
IonAir1 <ionair.ironwood@gmail.com>
2023-08-17 13:18:58 +0800

    added a range option

--- idmaker.py
+++ idmaker.py
@@ -83,8 +83,12 @@ def generate_id(template, name, picture, qrcode):
 
 def generate_ids():
     names = compile_names(settings.EXCEL)
+    if settings.RANGE > 0:
+        names = names[:settings.RANGE]
     str_names = "\n".join(names)
+
     qrcodes = generate_qr(str_names)
+    
     pictures = load_images(settings.EXCEL, settings.PICTURE, len(names))
 
     skipped = []
