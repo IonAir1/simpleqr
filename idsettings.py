3641c7b272482a44af0170875321caa35b47efa2
ionair <ionair.ironwood@gmail.com>
2023-09-02 10:43:20 +0800

    added option to include room number

--- idsettings.py
+++ idsettings.py
@@ -6,19 +6,23 @@ BORDER_SIZE = 0
 LINK = "www.google.com/=lastname=firstname"
 
 #excel settings
-EXCEL = "KAMIARES2023.xlsx"
+EXCEL = "input.xlsx"
 RANGE = 0
 NAME = 'B'
 MIDDLE_NAME = ''
 PICTURE = 'L'
+ROOM_NUMBER = 'A'
 
 #id settings
 TEMPLATE = "template.png"
 FONT = "Roboto-Regular.ttf"
-FONT_SIZE = 50
-FONT_COLOR = (123, 17, 29)
+NAME_SIZE = 50
+NAME_COLOR = (123, 17, 29)
 PICTURE_POS = (148, 445, 727, 692)
 ASPECT_RATIO = "keep"
 MISSING_PICTURE = "skip"
 QR_POS = (353, 1469, 301, 301)
 NAME_POS = (0, 1155, 1024, 50)
+RN_SIZE = 40
+RN_COLOR = (30, 30, 30)
+RN_POS = (0, 1300, 1024, 50)
