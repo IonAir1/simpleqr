40408a7636ba7988da944437068fb0cb74902cd8
IonAIr1 <ionair.ironwood@gmail.com>
2024-07-16 22:35:54 +0800

    add gen.py file that combines scrapetoexcel.py and simpleqr.py

---
+++ gen.py
@@ -0,0 +1,22 @@
+from simpleqr import IDMaker
+import scrapetoexcel
+import shutil
+
+
+file_path = "sample/Dorm.DILIMAN.html"
+
+
+def scrape():
+    scrapetoexcel.html_to_excel(file_path, "to_input.xlsx", skip_pdf=False)
+    inp = input("Proceed to QR Code Generation? y/enter=yes,r=retry,n=cancel \n")
+    if inp.lower() == 'r':
+        print("Retrying...")
+        scrape()
+    elif inp.lower() == 'n':
+        print("Cacnceled!")
+    elif inp.lower() in ['', 'y']:
+        print("Proceeding...")
+        IDMaker("idsettings.cfg").generate_ids()
+
+
+scrape()
